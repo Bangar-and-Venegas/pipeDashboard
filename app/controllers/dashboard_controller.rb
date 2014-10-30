d3ae424fc7cb3dca70999127d73b54ed778a948a
Alan Venegas <alan.noel.venegas@gmail.com>
2014-10-30 10:55:02 +0100

    Integrate Revenue/Sales_person graph

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,9 @@ class DashboardController < ApplicationController
 
 	def index
 		@users = User.all
+		@revenue_per_sales_person = {}
+		@users.each do |user|
+			@revenue_per_sales_person[user.name]=Deal.where(user_id: user).sum(:value)
+		end
 	end
 end
