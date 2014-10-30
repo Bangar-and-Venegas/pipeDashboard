2c82a74282551261bd94f55ef827450a3319b0ec
Alan Venegas <alan.noel.venegas@gmail.com>
2014-10-30 12:25:31 +0100

    Refactor tasks

--- lib/task_helper.rb
+++ lib/task_helper.rb
@@ -4,57 +4,64 @@ module TaskHelper
 	require 'json'
 
 	def populate_users
-		response_in_hash_format = get_hash_from_api("users", ":(id,name,created,modified)", "")
-		load_users(response_in_hash_format)
+		table = "users"
+		filters = ":(id,name,created,modified)"
+		params = ""
+		hash_response = get_hash_from_api(table, filters, params)
+		load_users(hash_response)
 	end
 
 	def populate_deals
-		response_in_hash_format = get_hash_from_api("deals",
-																								":(id,user_id,title,value,currency,status,won_time,add_time,update_time)", "")
-		load_deals(response_in_hash_format)
+		table = "deals"
+		filters = ":(id,user_id,title,value,currency,status,won_time,add_time,update_time)"
+		params = "filter_id=2&" # Only won deals
+		hash_response = get_hash_from_api(table, filters, params)
+		load_deals(hash_response)
 	end
 
 	def populate_activities
-		response_in_hash_format = get_hash_from_api("activities",
-																								":(id,add_time,update_time,user_id,deal_id,type,done,note,due_time)",
-																								"user_id=361107&start=0&")
-		load_activities(response_in_hash_format)
+		Activity.delete_all
+		table = "activities"
+		filters = ":(id,add_time,update_time,user_id,deal_id,type,done,note,due_time)"
+		User.all.each do |user|
+			params = "user_id=#{user.id}&done=1&" # Only done activities
+			hash_response = get_hash_from_api(table, filters, params)
+			load_activities(hash_response)
+		end
 	end
 
 	def get_hash_from_api (model, filter, params)
 		api_base_url = "https://api.pipedrive.com/v1/"
 		api_token = Rails.application.secrets.pipedrive_api_key
 		api_url = "#{api_base_url}#{model}#{filter}?#{params}api_token=#{api_token}"
-		puts api_url
 		response_in_json_format =RestClient.get(api_url)
 		JSON.parse(response_in_json_format)
 	end
 
-	def load_users (response_in_hash_format)
+	def load_users (hash_response)
 		User.delete_all
-		unformatted_users= response_in_hash_format["data"]
+		unformatted_users= hash_response["data"]
 		unformatted_users.each do |user|
-			User.create(name: user['name'], id: user['id'], created: user['created'], modified: user['modified'])
+			User.create(user)
 		end
 	end
 
-	def load_deals (response_in_hash_format)
+	def load_deals (hash_response)
 		Deal.delete_all
-		unformatted_deals = response_in_hash_format["data"]
+		unformatted_deals = hash_response["data"]
 		unformatted_deals.each do |deal|
-			Deal.create(id: deal['id'],user_id: deal['user_id']['id'], title: deal['title'], value: deal['value'],
-									currency: deal['currency'], status: deal['status'], won_time: deal['won_time'],
-									add_time: deal['add_time'], update_time: deal['update_time'])
+			deal[:user_id]=deal['user_id']['id']
+			deal.delete('user_id')
+			Deal.create(deal)
 		end
 	end
 
-	def load_activities (response_in_hash_format)
-		Activity.delete_all
-		unformatted_activities = response_in_hash_format["data"]
+	def load_activities (hash_response)
+		unformatted_activities = hash_response["data"]
 		unformatted_activities.each do |activity|
-			Activity.create(id: activity['id'], add_time: activity['add_time'], update_time: activity['update_time'],
-											user_id: activity['user_id'], deal_id: activity['deal_id'], activity_type: activity['type'],
-											done: activity['done'],	note: activity['note'], due_time: activity['due_time'])
+			activity[:activity_type]= activity['type']
+			activity.delete('type')
+			Activity.create(activity)
 		end
 	end
 
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
-  describe 'GET #index' do
-    it 'returns a list of restaurants' do
-      get :index
-      expect(response).to be_success
-      expect(assigns(:users)).to eq(User.all)
-    end
-  end
-  
+	describe 'GET #index' do
+		before do
+			get :index
+		end
+
+		it 'gives a successful response' do
+			expect(response).to be_success
+		end
+
+		it 'creates a list of users' do
+			expect(assigns(:users)).to eq(User.all)
+		end
+
+		it 'creates a hash with revenue per user' do
+			expect(assigns(:revenue_per_sales_person)).not_to be_nil
+		end
+	end
 end
