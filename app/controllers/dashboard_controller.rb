290e8517c530c778f837de37a7ef35bca36bcbcc
Alan Venegas <alan.noel.venegas@gmail.com>
2014-11-04 14:54:29 +0100

    Completed info in dashboard graphs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,8 @@ class DashboardController < ApplicationController
 		@revenue_per_person = {}
 		@activities_per_person = {}
 		@number_of_sales_per_person = {}
+		@activities_per_person_per_day = {}
+		@average_revenue_per_person = {}
 
 		@users.each do |user|
 			sales_value = {}
@@ -22,11 +24,25 @@ class DashboardController < ApplicationController
 			sales_number[:year]=Deal.where(user_id: user, won_time: 1.year.ago..Time.now).count
 			@number_of_sales_per_person[user.name]=sales_number
 
+			average_revenue = {}
+			average_revenue[:total]=sales_value[:total]/sales_number[:total]
+			average_revenue[:month]=sales_value[:month]/sales_number[:total]
+			average_revenue[:quarter]=sales_value[:quarter]/sales_number[:quarter]
+			average_revenue[:year]=sales_value[:year]/sales_number[:year]
+			@average_revenue_per_person[user.name]=average_revenue
+
+
 			activities = {}
+			activities_per_day = {}
 			activities[:total]=Activity.where(user_id: user).count
 			activities[:month]=Activity.where(user_id: user, marked_as_done_time: 1.month.ago..Time.now).count
+			activities_per_day[:month]=activities[:month]/30.0
+			activities[:quarter]=Activity.where(user_id: user, marked_as_done_time: 3.month.ago..Time.now).count
+			activities_per_day[:quarter]=activities[:quarter]/(Time.now - 1.month.ago)
 			activities[:year]=Activity.where(user_id: user, marked_as_done_time: 1.year.ago..Time.now).count
+			activities_per_day[:year]=activities[:year]/(Time.now - 1.year.ago)
 			@activities_per_person[user.name]=activities
+			@activities_per_person_per_day[user.name]=activities_per_day
 		end
 	end
 end
