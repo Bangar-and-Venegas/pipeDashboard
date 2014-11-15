7a7f03506ed64303467f203437ecdda93a02ccf3
Alan Venegas <alan.noel.venegas@gmail.com>
2014-11-16 00:15:26 +0100

    Add panel title and refactor

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,12 +3,6 @@ class DashboardController < ApplicationController
 	def index
 		@users = User.all
 
-		@revenue_per_person = {}
-		@activities_per_person = {}
-		@number_of_sales_per_person = {}
-		@average_revenue_per_person = {}
-		@call_conversion_rate_per_person = {}
-
 		@users_data = []
 
 		colors = generate_colors
@@ -20,29 +14,24 @@ class DashboardController < ApplicationController
 			sales_value[:month]=user.value_of_deals(1.month.ago)
 			sales_value[:quarter]=user.value_of_deals(3.month.ago)
 			sales_value[:year]=user.value_of_deals(1.year.ago)
-			@revenue_per_person[user.name]=sales_value
 
 			sales_number = {}
 			sales_number[:total]=user.number_of_deals
 			sales_number[:month]=user.number_of_deals(1.month.ago)
 			sales_number[:quarter]=user.number_of_deals(3.month.ago)
 			sales_number[:year]=user.number_of_deals(1.year.ago)
-			@number_of_sales_per_person[user.name]=sales_number
 
 			average_revenue = {}
 			average_revenue[:total]=user.average_revenue
 			average_revenue[:month]=user.average_revenue(1.month.ago)
 			average_revenue[:quarter]=user.average_revenue(3.month.ago)
 			average_revenue[:year]=user.average_revenue(1.year.ago)
-			@average_revenue_per_person[user.name]=average_revenue
-
 
 			activities = {}
 			activities[:total]=user.number_of_activities
 			activities[:month]=user.number_of_activities(1.month.ago)
 			activities[:quarter]=user.number_of_activities(3.month.ago)
 			activities[:year]=user.number_of_activities(1.year.ago)
-			@activities_per_person[user.name]=activities
 
 			activities_per_day = {}
 			activities_per_day[:month]=user.activities_per_day(1.month.ago)
@@ -54,7 +43,6 @@ class DashboardController < ApplicationController
 			call_conversion[:month]=user.call_conversion_rate(1.month.ago)
 			call_conversion[:quarter]=user.call_conversion_rate(3.month.ago)
 			call_conversion[:year]=user.call_conversion_rate(1.year.ago)
-			@call_conversion_rate_per_person[user.name] = call_conversion
 
 			user_data= {}
 			user_data[:name]=user.name
